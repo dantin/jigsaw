355070b27a4f6076daba125a29239a586888b2c9
dantin <chengjie.ding@gmail.com>
2016-12-12 23:35:53 +0800

    ClassLoaderTest

---
+++ java/test/ClassLoaderTest.java
@@ -0,0 +1,13 @@
+import java.net.URL;
+
+public class ClassLoaderTest {
+
+    public static void main(String[] args) {
+        URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+        for(int i = 0; i < urls.length; i++) {
+            System.out.println(urls[i].toExternalForm());
+        }
+
+        System.out.println(System.getProperty("sun.boot.class.path"));
+    }
+}
