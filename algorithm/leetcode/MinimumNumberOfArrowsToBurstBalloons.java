69780c57b991bbe3dba14e05e82af567e7671073
dantin <chengjie.ding@gmail.com>
2016-12-23 14:49:30 +0800

    refactoring var

--- algorithm/leetcode/MinimumNumberOfArrowsToBurstBalloons.java
+++ algorithm/leetcode/MinimumNumberOfArrowsToBurstBalloons.java
@@ -4,16 +4,16 @@ public class MinimumNumberOfArrowsToBurstBalloons {
 
     public int findMinArrowShots(int[][] points) {
         if (points == null || points.length == 0) return 0;
-
+        // sort by begin, then end
         Arrays.sort(points, (x, y) -> x[0] == y[0] ? x[1] - y[1] : x[0] - y[0]);
         int count = 1;
-        int limit = points[0][1];
+        int end = points[0][1];
         for (int i = 1; i < points.length; i++) {
-            if (points[i][0] <= limit) {
-                limit = Math.min(limit, points[i][1]);
+            if (points[i][0] <= end) {
+                end = Math.min(end, points[i][1]);
             } else {
                 count++;
-                limit = points[i][1];
+                end = points[i][1];
             }
         }
         return count;
