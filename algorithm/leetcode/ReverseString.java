afe5287812e28a80cefc467ec84ddca02ba2c1d4
dantin <chengjie.ding@gmail.com>
2017-01-03 15:33:10 +0800

    add solution of reverse string

--- algorithm/leetcode/ReverseString.java
+++ algorithm/leetcode/ReverseString.java
@@ -1,8 +1,25 @@
 public class ReverseString {
 
+    public String reverseString2(String s) {
+        StringBuilder buffer = new StringBuilder(s);
+        return buffer.reverse().toString();
+    }
+
     public String reverseString(String s) {
+        char[] buffer = s.toCharArray();
+        int i = 0, j = buffer.length - 1;
+        while (i < j) {
+            char tmp = buffer[i];
+            buffer[i++] = buffer[j];
+            buffer[j--] = tmp;
+        }
+
+        return new String(buffer);
+    }
+
+    public String reverseString1(String s) {
         StringBuilder buffer = new StringBuilder();
-        for(int i = s.length() - 1; i >= 0; i--) {
+        for (int i = s.length() - 1; i >= 0; i--) {
             buffer.append(s.charAt(i));
         }
 
