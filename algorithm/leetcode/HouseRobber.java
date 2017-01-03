e2d9dce4f5ecc5734893108db9e010b04ab55966
dantin <chengjie.ding@gmail.com>
2017-01-03 11:29:57 +0800

    add solution of house robber

--- algorithm/leetcode/HouseRobber.java
+++ algorithm/leetcode/HouseRobber.java
@@ -1,16 +1,28 @@
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class HouseRobber {
 
+    public int rob1(int[] nums) {
+        if (nums.length <= 1) return nums.length == 0 ? 0 : nums[0];
+        LinkedList<Integer> dp = new LinkedList<>();
+        dp.add(nums[0]);
+        dp.add(Math.max(nums[0], nums[1]));
+        for (int i = 2; i < nums.length; i++) {
+            dp.add(Math.max(nums[i] + dp.get(i - 2), dp.peekLast()));
+        }
+        return dp.peekLast();
+    }
+
     public int rob(int[] nums) {
-        if(nums == null || nums.length == 0) return 0;
-        if(nums.length == 1) return nums[0];
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
         int res = Math.max(nums[0], nums[1]);
-        if(nums.length == 2) return res;
+        if (nums.length == 2) return res;
 
         int hist = nums[0];
         int pre = res;
-        for(int i = 2; i < nums.length; i++) {
+        for (int i = 2; i < nums.length; i++) {
             res = Math.max(hist + nums[i], pre);
             hist = pre;
             pre = res;
@@ -20,7 +32,7 @@ public class HouseRobber {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{5, 10, 1, 23, 8};
+        int[] nums = new int[] {5, 10, 1, 23, 8};
 
         HouseRobber solution = new HouseRobber();
         System.out.println(Arrays.toString(nums));
