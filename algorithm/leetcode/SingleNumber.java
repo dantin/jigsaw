cab14dddd1cc5ccd98d61371767160d61bdc6492
dantin <chengjie.ding@gmail.com>
2017-01-03 16:09:11 +0800

    refactoring SingleNumber.java

--- algorithm/leetcode/SingleNumber.java
+++ algorithm/leetcode/SingleNumber.java
@@ -2,7 +2,7 @@ public class SingleNumber {
 
     public int singleNumber(int [] nums) {
         int target = 0;
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             target ^= nums[i];
         }
         return target;
@@ -10,6 +10,6 @@ public class SingleNumber {
 
     public static void main(String[] args) {
         SingleNumber solution = new SingleNumber();
-        System.out.println(solution.singleNumber(new int[]{5, 3, 6, 9, 5, 9, 3}));
+        System.out.println(solution.singleNumber(new int[] {5, 3, 6, 9, 5, 9, 3}));
     }
 }
