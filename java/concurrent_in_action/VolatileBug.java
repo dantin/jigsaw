846e2a935defedf73e58ca0b8821ec1de50dbca8
dantin <chengjie.ding@gmail.com>
2017-02-07 14:51:17 +0800

    volatile only provide accessibility and ordering

---
+++ java/concurrent_in_action/VolatileBug.java
@@ -0,0 +1,26 @@
+public class VolatileBug {
+    static volatile int i = 0;
+
+    public static class PlusTask extends Thread {
+        @Override
+        public void run() {
+            for (int k = 0; k < 10000; k++)
+                i++;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[10];
+
+        for (int i = 0; i < 10; i++) {
+            threads[i] = new Thread(new PlusTask());
+            threads[i].start();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            threads[i].join();
+        }
+
+        System.out.println(i);
+    }
+}
