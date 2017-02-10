4d2e49e76a8a157af3534a9c8b013262f41146fd
dantin <chengjie.ding@gmail.com>
2017-02-10 14:48:10 +0800

    RejectPolicy in ThreadPoolExecutor

---
+++ java/concurrent_in_action/RejectThreadPoolDemo.java
@@ -0,0 +1,37 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class RejectThreadPoolDemo {
+    public static class MyTask implements Runnable {
+        @Override
+        public void run() {
+            System.out.println(System.currentTimeMillis() + ":Thread ID:" + Thread.currentThread().getId());
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MyTask r = new MyTask();
+        ExecutorService exec = new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>(10),
+                Executors.defaultThreadFactory(),
+        new RejectedExecutionHandler() {
+            @Override
+            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+                System.out.println(r.toString() + " is discard");
+            }
+        });
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            exec.submit(r);
+            Thread.sleep(10);
+        }
+    }
+}
