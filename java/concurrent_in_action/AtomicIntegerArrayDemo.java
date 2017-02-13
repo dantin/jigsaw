76a3539c9d0d188a11fd3e96266ad01790364e4a
dantin <chengjie.ding@gmail.com>
2017-02-13 16:01:29 +0800

    AtomicIntegerArray demo

---
+++ java/concurrent_in_action/AtomicIntegerArrayDemo.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+public class AtomicIntegerArrayDemo {
+    static AtomicIntegerArray array = new AtomicIntegerArray(10);
+
+    public static class AddThread implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                array.getAndIncrement(i % array.length());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] ts = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            ts[i] = new Thread(new AddThread());
+        }
+
+        for (int i = 0; i < 10; i++) {
+            ts[i].start();
+        }
+        for (int i = 0; i < 10; i++) {
+            ts[i].join();
+        }
+        System.out.println(array);
+    }
+}
