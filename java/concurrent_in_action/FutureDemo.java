dfa75df8612daad29d5d7026c8ee14857a70f35d
dantin <chengjie.ding@gmail.com>
2017-02-14 15:21:04 +0800

    Future pattern

---
+++ java/concurrent_in_action/FutureDemo.java
@@ -0,0 +1,80 @@
+public class FutureDemo {
+    public static void main(String[] args) {
+        Client client = new Client();
+
+        Data data = client.request("name");
+        System.out.println("请求完毕");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("数据: %s\n", data.getResult());
+    }
+}
+
+interface Data {
+    public String getResult();
+}
+
+class RealData implements Data {
+    protected final String result;
+
+    public RealData(String para) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < 10; i++) {
+            sb.append(para);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        result = sb.toString();
+    }
+
+    @Override
+    public String getResult() {
+        return result;
+    }
+}
+
+class FutureData implements Data {
+    protected RealData realData = null;
+    protected boolean isReady = false;
+
+    public synchronized void setRealData(RealData realData) {
+        if (isReady) {
+            return;
+        }
+        this.realData = realData;
+        isReady = true;
+        notifyAll();
+    }
+
+    @Override
+    public synchronized String getResult() {
+        while (!isReady) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return realData.result;
+    }
+}
+
+class Client {
+    public Data request(final String str) {
+        final FutureData future = new FutureData();
+        new Thread() {
+            @Override
+            public void run() {
+                RealData real = new RealData(str);
+                future.setRealData(real);
+            }
+        } .start();
+        return future;
+    }
+}
