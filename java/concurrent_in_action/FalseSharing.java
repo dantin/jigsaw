a3280ffe8d28fbe759b983ad07b4fb8cc87486e4
dantin <chengjie.ding@gmail.com>
2017-02-14 14:32:03 +0800

    demo of cache line

---
+++ java/concurrent_in_action/FalseSharing.java
@@ -0,0 +1,52 @@
+public class FalseSharing implements Runnable {
+
+    public static final int NUM_THREADS = 2;
+    public static final long ITERATIONS = 500L * 1000L * 1000L;
+    private final int arrayIndex;
+
+    private static VolatileLong[] longs = new VolatileLong[NUM_THREADS];
+    static {
+        for (int i = 0; i < NUM_THREADS; i++) {
+            longs[i] = new VolatileLong();
+        }
+    }
+
+    public final static class VolatileLong {
+        public volatile long value = 0L;
+        public long p1, p2, p3, p4, p5, p6, p7;
+    }
+
+    public FalseSharing(final int arrayIndex) {
+        this.arrayIndex = arrayIndex;
+    }
+
+    @Override
+    public void run() {
+        long i = ITERATIONS + 1;
+        while (0 != --i) {
+            longs[arrayIndex].value = i;
+        }
+    }
+
+    private static void runTest() throws InterruptedException {
+        Thread[] threads = new Thread[NUM_THREADS];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new FalseSharing(i));
+        }
+
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            t.join();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final long start = System.currentTimeMillis();
+        runTest();
+        System.out.printf("duration = %d\n", (System.currentTimeMillis() - start));
+    }
+}
