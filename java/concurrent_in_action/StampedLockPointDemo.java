0dee6297c7090af78dbe0a5aa4c2dc9cbee1dd75
dantin <chengjie.ding@gmail.com>
2017-02-20 07:33:55 +0800

    StampedLock demo

---
+++ java/concurrent_in_action/StampedLockPointDemo.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.locks.StampedLock;
+
+public class StampedLockPointDemo {
+    private double x, y;
+    private final StampedLock sl = new StampedLock();
+
+    public void move(double deltaX, double deltaY) {
+        long stamp = sl.writeLock();
+        try {
+            x += deltaX;
+            y += deltaY;
+        } finally {
+            sl.unlockWrite(stamp);
+        }
+    }
+
+    public double distance() {
+        long stamp = sl.tryOptimisticRead();
+        double currentX = x, currentY = y;
+        if (!sl.validate(stamp)) {
+            stamp = sl.readLock();
+            try {
+                currentX = x;
+                currentY = y;
+            } finally {
+                sl.unlockRead(stamp);
+            }
+        }
+        return Math.sqrt(currentX * currentX + currentY * currentY);
+    }
+}
