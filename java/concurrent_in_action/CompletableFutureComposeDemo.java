ca8809648a0a204e42a00c52fded0aabd22c17a6
dantin <chengjie.ding@gmail.com>
2017-02-20 07:16:57 +0800

    compose using CompletableFuture

---
+++ java/concurrent_in_action/CompletableFutureComposeDemo.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureComposeDemo {
+
+    public static Integer calc(Integer para) {
+        return para / 2;
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> calc(50))
+                                               .thenCompose((i) -> CompletableFuture.supplyAsync(() -> calc(i)))
+                                               .thenApply((i) -> Integer.toString(i))
+                                               .thenApply((str) -> "\"" + str + "\"")
+                                               .thenAccept(System.out::println);
+        future.get();
+    }
+}
