a809f1b6b447db96645eab3bcbf45abd5c6fa238
dantin <chengjie.ding@gmail.com>
2017-02-27 14:00:59 +0800

    Demo of OOM dump

---
+++ java/jvm_in_action/DumpOOM.java
@@ -0,0 +1,10 @@
+import java.util.Vector;
+
+public class DumpOOM {
+    public static void main(String[] args) {
+        Vector<byte[]> v = new Vector<>();
+        for (int i = 0; i < 25; i++) {
+            v.add(new byte[1 * 1024 * 1024]);
+        }
+    }
+}
