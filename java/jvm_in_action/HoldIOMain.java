1339a303410aac6bd05fba31314646e6a8cf2925
dantin <chengjie.ding@gmail.com>
2017-03-07 15:47:47 +0800

    monitor using hprof

--- java/jvm_in_action/HoldIOMain.java
+++ java/jvm_in_action/HoldIOMain.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-
 public class HoldIOMain {
     public static class HoldIOTask implements Runnable {
         @Override
