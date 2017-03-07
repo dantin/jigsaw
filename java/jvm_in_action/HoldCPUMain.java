c4660a16920d7473a871c8f3a73b734e4eda14ae
dantin <chengjie.ding@gmail.com>
2017-03-07 09:54:07 +0800

    thread using 100% CPU

---
+++ java/jvm_in_action/HoldCPUMain.java
@@ -0,0 +1,30 @@
+public class HoldCPUMain {
+    public static class HoldCPUTask implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                double a = Math.random() * Math.random();
+            }
+        }
+    }
+
+    public static class LazyTask implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new HoldCPUTask()).start();
+        new Thread(new LazyTask()).start();
+        new Thread(new LazyTask()).start();
+        new Thread(new LazyTask()).start();
+    }
+}
